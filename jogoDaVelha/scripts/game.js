2ff729f9675afd1cde41ac63418c1c6af35e9b50
Jean Luiz <zatzk@outlook.com>
2022-03-06 13:12:20 -0300

    comments

--- jogoDaVelha/scripts/game.js
+++ jogoDaVelha/scripts/game.js
@@ -1,5 +1,5 @@
 // iniciar minhas variáveis
-let board = ['', '', '', '', '', '', '', '', ''];
+let board = ['', '', '', '', '', '', '', '', '']; 
 let playerTime = 0;
 let symbols = ['x', 'o'];
 let gameOver = false;
@@ -14,17 +14,17 @@ let winStates =[
   [2, 4, 6]
 ];
 
-function handleMove(position) {
-  if(gameOver) {
-    return;
+function handleMove(position) { //movimento
+  if(gameOver) { //se o jogo acabou
+    return; //retorna
   }
 
 
-  if(board[position] == '') {
-    board[position] = symbols[playerTime];
+  if(board[position] == '') { //se a posição estiver vazia
+    board[position] = symbols[playerTime]; //insere o símbolo na posição
 
-    gameOver = checkWin();
-    if(!gameOver) {
+    gameOver = checkWin(); //verifica se o jogo acabou
+    if(!gameOver) { //se o jogo não acabou
       playerTime = playerTime == 0 ? 1 : 0; // alterna o jogador
     }
   }
@@ -32,19 +32,19 @@ function handleMove(position) {
   return gameOver;
 }
 
-function checkWin() {
-  for(let i = 0; i < winStates.length; i++) {
-    let seq = winStates[i];
+function checkWin() { //verifica se o jogador ganhou
+  for(let i = 0; i < winStates.length; i++) { //percorre todos os estados de vitória
+    let seq = winStates[i]; //pega o estado de vitória
 
-    let pos0 = seq[0];
+    let pos0 = seq[0]; 
     let pos1 = seq[1];
     let pos2 = seq[2];
 
-    if(board[pos0] == board[pos1] &&
+    if(board[pos0] == board[pos1] && 
        board[pos0] == board[pos2] &&
        board[pos0] != '') {	
-      return true;
+      return true; //retorna true se o jogador ganhou
     }
   }
-  return false;
+  return false; //retorna false se o jogador não ganhou
 }
--- jogoDaVelha/scripts/interface.js
+++ jogoDaVelha/scripts/interface.js
@@ -1,39 +1,39 @@
-document.getElementById("btn-reset").addEventListener("click", resetGame);
-document.addEventListener("DOMContentLoaded", () => {
-  let squares = document.querySelectorAll(".square");
-  squares.forEach(square => {
-    square.addEventListener('click', handleClick);
+document.getElementById("btn-reset").addEventListener("click", resetGame); //adiciona o evento de click no botão reset
+document.addEventListener("DOMContentLoaded", () => { //quando o DOM estiver carregado
+  let squares = document.querySelectorAll(".square"); //pega todos os elementos do tipo square
+  squares.forEach(square => {  //percorre todos os elementos
+    square.addEventListener('click', handleClick); //adiciona o evento de click
   }); 
 });
 
 
-function handleClick(event) {
-  let square = event.target;
-  let position = square.id;
+function handleClick(event) { //evento de click
+  let square = event.target; //pega o elemento que foi clicado
+  let position = square.id; //pega a posição do elemento clicado
   
-  if(handleMove(position)) {
-    setTimeout(() => {
-      alert("Jogador " + (playerTime + 1) + " ganhou!");
-    }, 10);
+  if(handleMove(position)) { //se o movimento for válido
+    setTimeout(() => { //espera um tempo para atualizar o tabuleiro
+      alert("Jogador " + (playerTime + 1) + " ganhou!"); //alerta o vencedor
+    }, 10); //tempo de espera
   }
 
-  updateSquare(position);
+  updateSquare(position); //atualiza o tabuleiro
 }
-function updateSquare(position) {
-  let square = document.getElementById(position.toString());
-  let symbol = board[position];
-  square.innerHTML = `<div class=${symbol}></div>`;
+function updateSquare(position) { //atualiza o tabuleiro
+  let square = document.getElementById(position.toString()); //pega o elemento que foi clicado
+  let symbol = board[position]; //pega o símbolo do elemento clicado
+  square.innerHTML = `<div class=${symbol}></div>`; //insere o símbolo no elemento clicado
 
 }
-function resetGame() {
-  board = ['', '', '', '', '', '', '', '', ''];
-  playerTime = 0;
+function resetGame() { //reseta o jogo
+  board = ['', '', '', '', '', '', '', '', '']; 
+  playerTime = 0; 
   gameOver = false;
-  updateBoard();
+  updateBoard(); //atualiza o tabuleiro
 }
-function updateBoard() {
-  let squares = document.querySelectorAll(".square");
-  squares.forEach(square => {
-    square.innerHTML = '';
-  });
+function updateBoard() { //atualiza o tabuleiro
+  let squares = document.querySelectorAll(".square"); //pega todos os elementos do tipo square
+  squares.forEach(square => {   //percorre todos os elementos
+    square.innerHTML = ''; //limpa o elemento
+  }); 
 }
